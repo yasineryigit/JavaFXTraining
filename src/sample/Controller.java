6773d5a19e30c513acfb4660dcc58e3f128c7030
yasineryigit <yasineryigit737@gmail.com>
2021-05-13 00:41:44 +0300

    ColorPicker added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,28 +3,30 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import javafx.geometry.Insets;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 
 public class Controller {
 
     @FXML
-    private Label myLabel;
+    AnchorPane myPane;
 
     @FXML
-    private DatePicker myDatePicker;
+    ColorPicker myColorPicker;
 
-
-    public void getDate(ActionEvent event){
-        LocalDate myDate = myDatePicker.getValue();
-        String myFormattedDate = myDate.format(DateTimeFormatter.ofPattern("MMM-dd-yyy"));
-        myLabel.setText(myFormattedDate);
+    public void changeColor(ActionEvent e){
+        Color myColor = myColorPicker.getValue();
+        myPane.setBackground(new Background(new BackgroundFill(myColor, CornerRadii.EMPTY, Insets.EMPTY)));
     }
+
+
+
 
 
 
