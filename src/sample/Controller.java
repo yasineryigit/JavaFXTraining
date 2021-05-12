abb00ef72c1c1b4ae9c854d5c27478eb24c127ce
yasineryigit <yasineryigit737@gmail.com>
2021-05-12 23:03:46 +0300

    Checkbox added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -15,29 +16,14 @@ public class Controller {
     @FXML
     private Label myLabel;
     @FXML
-    private TextField textFieldAge;
-    @FXML
-    private Button buttonSubmit;
+    private CheckBox myCheckBox;
 
-    int age;
-
-    public void submit(ActionEvent event) {
-        try{
-            age = Integer.parseInt(textFieldAge.getText());
-
-            if(age>=18){
-                myLabel.setText("You are now signed up!");
-            }else{
-                myLabel.setText("You must be 18+");
-            }
+    public void change(ActionEvent e){
+        if (myCheckBox.isSelected()) {
+            myLabel.setText("ON");
+        } else {
+            myLabel.setText("OFF");
         }
-        catch (NumberFormatException e){
-            myLabel.setText("Enter only number please");
-        }
-        catch (Exception e){
-            myLabel.setText("Error: " + e.toString());
-        }
-
     }
 
 
