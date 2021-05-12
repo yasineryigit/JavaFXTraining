792c4747f2423b2391540e451acae42ddbff3bf2
yasineryigit <yasineryigit737@gmail.com>
2021-05-12 21:10:43 +0300

    ImageView added | Image changes when mouse entered

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,38 +1,28 @@
 package sample;
 
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class Controller {
 
     @FXML
-    private Button logoutButton;
+    ImageView myImageView;
+    Button myButton;
 
-    @FXML
-    private AnchorPane anchorPane;
+    Image image1  = new Image(getClass().getResourceAsStream("hulk1.jpg"));
+    Image image2  = new Image(getClass().getResourceAsStream("hulk2.jpg"));
 
-    Stage stage;
-
-    public void logout(ActionEvent event){
-
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Logout");
-        alert.setHeaderText("You're about to logout");
-        alert.setContentText("Do you want to save before existing?");
-
-        if(alert.showAndWait().get()== ButtonType.OK){
-            //kapatmak istediğimiz stage'i alacağız
-            stage = (Stage) anchorPane.getScene().getWindow();
-            stage.close();
+    public void displayImage(){
+        if(myImageView.getImage()!=image1){
+            myImageView.setImage(image1);
+        }else{
+            myImageView.setImage(image2);
         }
+
     }
 
 
