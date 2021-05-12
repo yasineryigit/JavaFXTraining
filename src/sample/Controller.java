bf07d7d73e90526621d0819edec6070b320d9217
yasineryigit <yasineryigit737@gmail.com>
2021-05-13 00:32:57 +0300

    DatePicker used with DateTimeFormatter

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,28 +3,31 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.RadioButton;
 
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class Controller {
 
     @FXML
     private Label myLabel;
-    @FXML
-    private CheckBox myCheckBox;
 
-    public void change(ActionEvent e){
-        if (myCheckBox.isSelected()) {
-            myLabel.setText("ON");
-        } else {
-            myLabel.setText("OFF");
-        }
+    @FXML
+    private DatePicker myDatePicker;
+
+
+    public void getDate(ActionEvent event){
+        LocalDate myDate = myDatePicker.getValue();
+        String myFormattedDate = myDate.format(DateTimeFormatter.ofPattern("MMM-dd-yyy"));
+        myLabel.setText(myFormattedDate);
     }
+
+
+
 
 
 }
