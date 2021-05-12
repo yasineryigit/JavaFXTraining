04a9f07af3fd49177717092c518aed804022d5f1
yasineryigit <yasineryigit737@gmail.com>
2021-05-12 17:24:02 +0300

    Css styles defined for label and #labelTitle parts

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,27 +7,6 @@ import javafx.scene.shape.Circle;
 
 public class Controller {
 
-    @FXML
-    Circle blueCircle;
-    int X = 0 ;
-    int Y = 0 ;
-
-    public void up(ActionEvent e){
-        System.out.println("UP");
-        blueCircle.setCenterY(Y-=10);
-    }
-    public void down(ActionEvent e){
-        System.out.println("DOWN");
-        blueCircle.setCenterY(Y+=10);
-    }
-    public void right(ActionEvent e){
-        System.out.println("RIGHT");
-        blueCircle.setCenterX(X+=10);
-    }
-    public void left(ActionEvent e){
-        System.out.println("LEFT");
-        blueCircle.setCenterX(X-=10);
-    }
 
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,8 +14,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        String css = this.getClass().getResource("application.css").toExternalForm();//aynı css dosyasını birden fazla yerde kullanmak istersek
+        scene.getStylesheets().add(css);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
