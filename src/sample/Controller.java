97d9516e0db49ca83f51919e77b5dd7e3a491a2e
yasineryigit <yasineryigit737@gmail.com>
2021-05-14 01:10:08 +0300

    Spinner added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,15 @@
 package sample;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
 import java.math.BigDecimal;
 import java.net.URL;
@@ -14,28 +18,35 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    ProgressBar myProgressBar;
+    private Spinner<Integer> mySpinner;
 
     @FXML
-    Label myLabel;
+    private Label myLabel;
 
-    BigDecimal progress = new BigDecimal(String.format("%.2f",0.0));
+    int currentValue;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        myProgressBar.setStyle("-fx-accent: #e27878;");
-        myLabel.setText("Progress: "+ String.valueOf(progress));
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,10); //1 den 10 a kadar elemanları dizecek
+
+        valueFactory.setValue(1);
+        mySpinner.setValueFactory(valueFactory);
+        currentValue=mySpinner.getValue();
+
+        myLabel.setText(Integer.toString(currentValue));
+
+        mySpinner.valueProperty().addListener(new ChangeListener<Integer>() {
+            @Override
+            public void changed(ObservableValue<? extends Integer> observableValue, Integer integer, Integer t1) {
+
+                currentValue=mySpinner.getValue();
+
+                myLabel.setText(Integer.toString(currentValue));
+
+            }
+        });
     }
 
-    public void increaseProgress(ActionEvent e) {
-        if(progress.doubleValue()<1){ //progress tamamlanmadıysa arttır
-            progress = new BigDecimal(String.format("%.2f",progress.doubleValue()+0.1));
-            System.out.println(progress.doubleValue());
-            myProgressBar.setProgress(progress.doubleValue());
-            myLabel.setText("Progress: "+ ((int)Math.round(progress.doubleValue()*100))+"%");
-        }
 
-
-    }
 }
