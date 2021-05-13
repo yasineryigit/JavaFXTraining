73f9d8095c47c47e39ea719aef31c9ec9400dd64
yasineryigit <yasineryigit737@gmail.com>
2021-05-14 02:03:51 +0300

    MenuBar added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
@@ -11,45 +12,14 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    private TreeView treeView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        TreeItem<String> rootItem = new TreeItem<>("Files");
-
-        TreeItem<String> branchItem1 = new TreeItem<>("Pictures");
-        TreeItem<String> branchItem2 = new TreeItem<>("Videos");
-        TreeItem<String> branchItem3 = new TreeItem<>("Music");
-
-        TreeItem<String> leafItem1 = new TreeItem<>("Picture1");
-        TreeItem<String> leafItem2 = new TreeItem<>("Picture2");
-        TreeItem<String> leafItem3 = new TreeItem<>("Video1");
-        TreeItem<String> leafItem4 = new TreeItem<>("Video2");
-        TreeItem<String> leafItem5 = new TreeItem<>("Music1");
-        TreeItem<String> leafItem6 = new TreeItem<>("Music2");
-
-
-        branchItem1.getChildren().addAll(leafItem1, leafItem2);
-        branchItem2.getChildren().addAll(leafItem3, leafItem4);
-        branchItem3.getChildren().addAll(leafItem5, leafItem6);
-
-        rootItem.getChildren().addAll(branchItem1, branchItem2, branchItem3);
-
-        treeView.setRoot(rootItem);
     }
 
-
-    public void selectItem() {
-
-        TreeItem<String> item = (TreeItem) treeView.getSelectionModel().getSelectedItem();
-
-        if (item != null) {
-            System.out.println(item.getValue());
-        }
-
-
+    public void newMethod(){
+        System.out.println("You selected the new menu item");
     }
+
 
 }
