2264733671bb67838d16edae88ec62ca9be3c4b2
yasineryigit <yasineryigit737@gmail.com>
2021-05-14 00:57:11 +0300

    ProgressBar used with BigDecimal

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,36 +1,41 @@
 package sample;
 
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.ProgressBar;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
-    private Label myLabel;
+    ProgressBar myProgressBar;
 
     @FXML
-    private Slider mySlider;
+    Label myLabel;
 
-    int myTemperature;
-
+    BigDecimal progress = new BigDecimal(String.format("%.2f",0.0));
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        mySlider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                myTemperature = (int)mySlider.getValue();
-                myLabel.setText(String.valueOf(myTemperature));
-            }
-        });
+        myProgressBar.setStyle("-fx-accent: #e27878;");
+        myLabel.setText("Progress: "+ String.valueOf(progress));
+    }
+
+    public void increaseProgress(ActionEvent e) {
+        if(progress.doubleValue()<1){ //progress tamamlanmadıysa arttır
+            progress = new BigDecimal(String.format("%.2f",progress.doubleValue()+0.1));
+            System.out.println(progress.doubleValue());
+            myProgressBar.setProgress(progress.doubleValue());
+            myLabel.setText("Progress: "+ ((int)Math.round(progress.doubleValue()*100))+"%");
+        }
+
+
     }
 }
