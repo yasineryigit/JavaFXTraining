f8138d3f841e74ce5ab123eb01f6aa2517b0d629
yasineryigit <yasineryigit737@gmail.com>
2021-05-14 01:39:57 +0300

    TreeView added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,10 @@
 package sample;
 
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,28 +12,44 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    private ListView<String> myListView;
-
-    @FXML
-    private Label myLabel;
-
-    String[] food = {"pizza", "sushi", "burger"};
-
-    String currentFood;
+    private TreeView treeView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        myListView.getItems().addAll(food); //listView'a verileri string dizisi olarak veriyoruz
+        TreeItem<String> rootItem = new TreeItem<>("Files");
 
-        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                currentFood = myListView.getSelectionModel().getSelectedItem();
+        TreeItem<String> branchItem1 = new TreeItem<>("Pictures");
+        TreeItem<String> branchItem2 = new TreeItem<>("Videos");
+        TreeItem<String> branchItem3 = new TreeItem<>("Music");
 
-                myLabel.setText(currentFood);
-            }
-        });
+        TreeItem<String> leafItem1 = new TreeItem<>("Picture1");
+        TreeItem<String> leafItem2 = new TreeItem<>("Picture2");
+        TreeItem<String> leafItem3 = new TreeItem<>("Video1");
+        TreeItem<String> leafItem4 = new TreeItem<>("Video2");
+        TreeItem<String> leafItem5 = new TreeItem<>("Music1");
+        TreeItem<String> leafItem6 = new TreeItem<>("Music2");
+
+
+        branchItem1.getChildren().addAll(leafItem1, leafItem2);
+        branchItem2.getChildren().addAll(leafItem3, leafItem4);
+        branchItem3.getChildren().addAll(leafItem5, leafItem6);
+
+        rootItem.getChildren().addAll(branchItem1, branchItem2, branchItem3);
+
+        treeView.setRoot(rootItem);
+    }
+
+
+    public void selectItem() {
+
+        TreeItem<String> item = (TreeItem) treeView.getSelectionModel().getSelectedItem();
+
+        if (item != null) {
+            System.out.println(item.getValue());
+        }
+
+
     }
 
 }
