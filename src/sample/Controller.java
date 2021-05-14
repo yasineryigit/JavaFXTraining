7ade28445f58997e5fa5a0b050c335a87541a677
yasineryigit <yasineryigit737@gmail.com>
2021-05-14 03:12:59 +0300

    Animations added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,36 +1,70 @@
 package sample;
 
 
-import javafx.event.ActionEvent;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    @FXML
+    private ImageView imageView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
+        /*
+        //translate
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(imageView);
+        translate.setDuration(Duration.millis(1000));
+        translate.setCycleCount(TranslateTransition.INDEFINITE);
+        translate.setByX(250);
+        translate.setByY(-250);
+        translate.setAutoReverse(true);
+        translate.play();
 
-    public void moveUp(){
-        System.out.println("MOVING UP");
-    }
-    public void moveDown(){
-        System.out.println("MOVING DOWN");
-    }
-    public void moveLeft(){
-        System.out.println("MOVING LEFT");
-    }
-    public void moveRight(){
-        System.out.println("MOVING RIGHT");
-    }
+        //rotate
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(imageView);
+        rotate.setDuration(Duration.millis(1000));
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setInterpolator(Interpolator.LINEAR);
+        rotate.setByAngle(360);
+        rotate.setAxis(Rotate.Z_AXIS);
+        rotate.play();
 
 
+        //FADE
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(imageView);
+        fade.setDuration(Duration.millis(1000));
+        fade.setCycleCount(TranslateTransition.INDEFINITE);
+        fade.setInterpolator(Interpolator.LINEAR);
+        fade.setFromValue(1);
+        fade.setToValue(0);
+        fade.play();*/
+
+        //scale
+        ScaleTransition scale = new ScaleTransition();
+        scale.setNode(imageView);
+        scale.setDuration(Duration.millis(1000));
+        scale.setCycleCount(TranslateTransition.INDEFINITE);
+        scale.setInterpolator(Interpolator.LINEAR);
+        scale.setByX(2.0);
+        scale.setByY(2.0);
+        scale.setAutoReverse(true);
+        scale.play();
+
+
+
+    }
 
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,48 +16,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {//Açılışta sample1 scene'i gösteriyor
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene.fxml")); //loader tanımlandı
-        Parent root = loader.load();//root tanımlandı
-        Controller controller = loader.getController(); //loader üzerinden controller nesnesi oluşturuldu, bu controller nesnesi üzerinden o class'ın property ve metodlarına ulaşabiliriz.
-
-
-        Scene scene = new Scene(root);//scene tanımlandı
-
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-
-                switch (keyEvent.getCode()) {
-                    case W:
-                        controller.moveUp();
-                        break;
-                    case S:
-                        controller.moveDown();
-                        break;
-                    case A:
-                        controller.moveLeft();
-                        break;
-                    case D:
-                        controller.moveRight();
-                        break;
-                    default:
-                        System.out.println(keyEvent.getCode()+ " pressed");
-                        break;
-                }
-
-            }
-        });
-
-
+        FXMLLoader loader = new  FXMLLoader(getClass().getResource("Scene.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        primaryStage.setOnCloseRequest(event -> {
-            event.consume();
-            logout(primaryStage);
-        });//kapatma tuşuna basılınca logout metodunu çağıracak
+
+        primaryStage.setOnCloseRequest(event -> {logout(primaryStage);}); //stage'i kapatma isteğinde bu metod çalışır
+
 
     }
 
